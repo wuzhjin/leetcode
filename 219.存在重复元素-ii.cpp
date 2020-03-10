4deb9fedee572062f0d3815c03368244d4ce90b6
wuzhjin <wuzhjin@163.com>
2020-03-10 10:30:54 +0800

    tasks completed in 200310

---
+++ 219.存在重复元素-ii.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=219 lang=cpp
+ *
+ * [219] 存在重复元素 II
+ *
+ * https://leetcode-cn.com/problems/contains-duplicate-ii/description/
+ *
+ * algorithms
+ * Easy (36.18%)
+ * Likes:    145
+ * Dislikes: 0
+ * Total Accepted:    36.5K
+ * Total Submissions: 96.6K
+ * Testcase Example:  '[1,2,3,1]\n3'
+ *
+ * 给定一个整数数组和一个整数 k，判断数组中是否存在两个不同的索引 i 和 j，使得 nums [i] = nums [j]，并且 i 和 j
+ * 的差的绝对值最大为 k。
+ * 
+ * 示例 1:
+ * 
+ * 输入: nums = [1,2,3,1], k = 3
+ * 输出: true
+ * 
+ * 示例 2:
+ * 
+ * 输入: nums = [1,0,1,1], k = 1
+ * 输出: true
+ * 
+ * 示例 3:
+ * 
+ * 输入: nums = [1,2,3,1,2,3], k = 2
+ * 输出: false
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> m;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (m.count(nums[i]) && i - m[nums[i]] <= k) return true;
+            else m[nums[i]] = i;
+        }
+        return false;
+    }
+};
+// @lc code=end
+
