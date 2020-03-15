2c14b1ecb0b36334f73326df135dd3a2fd977f01
wuzhjin <wuzhjin@163.com>
2020-03-15 10:31:06 +0800

    200315

---
+++ 257.二叉树的所有路径.cpp
@@ -0,0 +1,63 @@
+/*
+ * @lc app=leetcode.cn id=257 lang=cpp
+ *
+ * [257] 二叉树的所有路径
+ *
+ * https://leetcode-cn.com/problems/binary-tree-paths/description/
+ *
+ * algorithms
+ * Easy (60.36%)
+ * Likes:    223
+ * Dislikes: 0
+ * Total Accepted:    28.3K
+ * Total Submissions: 45.1K
+ * Testcase Example:  '[1,2,3,null,5]'
+ *
+ * 给定一个二叉树，返回所有从根节点到叶子节点的路径。
+ * 
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 
+ * 示例:
+ * 
+ * 输入:
+ * 
+ * ⁠  1
+ * ⁠/   \
+ * 2     3
+ * ⁠\
+ * ⁠ 5
+ * 
+ * 输出: ["1->2->5", "1->3"]
+ * 
+ * 解释: 所有根节点到叶子节点的路径为: 1->2->5, 1->3
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> res;
+        if (root) helper(root, "", res);
+        return res;
+    }
+    void helper(TreeNode* node, string out, vector<string>& res) {
+        if (!node->left && !node->right)
+            res.push_back(out + to_string(node->val));
+        if (node->left)
+            helper(node->left, out + to_string(node->val) + "->", res);
+        if (node->right)
+            helper(node->right, out + to_string(node->val) + "->", res);
+    }
+};
+// @lc code=end
+
