8f2fcf14ff3c8a6e33f7708687ba182c49cea6f7
wuzhjin <wuzhjin@163.com>
2020-06-02 22:51:47 +0800

    200602

---
+++ 653.两数之和-iv-输入-bst.cpp
@@ -0,0 +1,78 @@
+/*
+ * @lc app=leetcode.cn id=653 lang=cpp
+ *
+ * [653] 两数之和 IV - 输入 BST
+ *
+ * https://leetcode-cn.com/problems/two-sum-iv-input-is-a-bst/description/
+ *
+ * algorithms
+ * Easy (54.95%)
+ * Likes:    138
+ * Dislikes: 0
+ * Total Accepted:    16.1K
+ * Total Submissions: 29.1K
+ * Testcase Example:  '[5,3,6,2,4,null,7]\n9'
+ *
+ * 给定一个二叉搜索树和一个目标结果，如果 BST 中存在两个元素且它们的和等于给定的目标结果，则返回 true。
+ * 
+ * 案例 1:
+ * 
+ * 
+ * 输入: 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 3   6
+ * ⁠/ \   \
+ * 2   4   7
+ * 
+ * Target = 9
+ * 
+ * 输出: True
+ * 
+ * 
+ * 
+ * 
+ * 案例 2:
+ * 
+ * 
+ * 输入: 
+ * ⁠   5
+ * ⁠  / \
+ * ⁠ 3   6
+ * ⁠/ \   \
+ * 2   4   7
+ * 
+ * Target = 28
+ * 
+ * 输出: False
+ * 
+ * 
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        unordered_set<int> st;
+        return helper(root, k, st);
+    }
+    bool helper(TreeNode* node, int k, unordered_set<int>& st) {
+        if (!node) return false;
+        if (st.count(k - node->val)) return true;
+        st.insert(node->val);
+        return helper(node->left, k, st) ||helper(node->right, k, st);
+    }
+};
+// @lc code=end
+
