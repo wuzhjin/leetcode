b42504bf61c82301f34a942141cca48be8e28bb4
wuzhjin <wuzhjin@163.com>
2020-06-09 08:33:49 +0800

    200609

---
+++ 680.验证回文字符串-ⅱ.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode.cn id=680 lang=cpp
+ *
+ * [680] 验证回文字符串 Ⅱ
+ *
+ * https://leetcode-cn.com/problems/valid-palindrome-ii/description/
+ *
+ * algorithms
+ * Easy (36.63%)
+ * Likes:    210
+ * Dislikes: 0
+ * Total Accepted:    42.9K
+ * Total Submissions: 108.9K
+ * Testcase Example:  '"aba"'
+ *
+ * 给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
+ * 
+ * 示例 1:
+ * 
+ * 
+ * 输入: "aba"
+ * 输出: True
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 
+ * 输入: "abca"
+ * 输出: True
+ * 解释: 你可以删除c字符。
+ * 
+ * 
+ * 注意:
+ * 
+ * 
+ * 字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+        while (left < right) {
+            if (s[left] != s[right]) return isValid(s, left, right - 1) || isValid(s, left + 1, right);
+            ++left; --right;
+        }
+        return true;
+    }
+    bool isValid(string s, int left, int right) {
+        while (left < right) {
+            if (s[left] != s[right]) return false;
+            ++left; -- right;
+        }
+        return true;
+    }
+};
+// @lc code=end
+
