1f33536f4c8cab18f83a0f2b7d400044414908e1
wuzhjin <wuzhjin@163.com>
2020-06-17 23:17:10 +0800

    200617

---
+++ 700.二叉搜索树中的搜索.java
@@ -0,0 +1,65 @@
+/*
+ * @lc app=leetcode.cn id=700 lang=java
+ *
+ * [700] 二叉搜索树中的搜索
+ *
+ * https://leetcode-cn.com/problems/search-in-a-binary-search-tree/description/
+ *
+ * algorithms
+ * Easy (73.71%)
+ * Likes:    62
+ * Dislikes: 0
+ * Total Accepted:    26.2K
+ * Total Submissions: 35.5K
+ * Testcase Example:  '[4,2,7,1,3]\n2'
+ *
+ * 给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+ * 
+ * 例如，
+ * 
+ * 
+ * 给定二叉搜索树:
+ * 
+ * ⁠       4
+ * ⁠      / \
+ * ⁠     2   7
+ * ⁠    / \
+ * ⁠   1   3
+ * 
+ * 和值: 2
+ * 
+ * 
+ * 你应该返回如下子树:
+ * 
+ * 
+ * ⁠     2     
+ * ⁠    / \   
+ * ⁠   1   3
+ * 
+ * 
+ * 在上述示例中，如果要找的值是 5，但因为没有节点值为 5，我们应该返回 NULL。
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        // if (root == null || val == root.val) return root;
+        // return val < root.val ? searchBST(root.left, val) : searchBST(root.right, val);
+        while (root != null && val != root.val) {
+            root = val < root.val ? root.left : root.right;
+        }
+        return root;
+    }
+}
+// @lc code=end
+
